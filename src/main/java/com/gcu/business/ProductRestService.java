fe01ed98232352b884566a53f9c4e697fd2d5ccf
masonblaut <masonblaut@gmail.com>
2022-08-17 21:02:37 -0700

    Milestone 7

---
+++ src/main/java/com/gcu/business/ProductRestService.java
@@ -0,0 +1,56 @@
+package com.gcu.business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gcu.model.ProductList;
+import com.gcu.model.ProductModel;
+
+
+@RestController
+@RequestMapping("/service")
+public class ProductRestService {
+	
+	@Autowired
+	ProductBusinessInterface service;
+	
+	@GetMapping(path="/getjson", produces= {MediaType.APPLICATION_JSON_VALUE})
+	public List<ProductModel> getProductsAsJson()
+	{
+		return service.getProducts();
+	}
+	
+	@GetMapping(path="/getxml", produces= {MediaType.APPLICATION_XML_VALUE})
+	public ProductList getProductsAsXml()
+	{
+		ProductList list = new ProductList();
+		list.setOrders(service.getProducts());
+		return list;
+	}
+	
+	@GetMapping(path="/getproduct/{id}")
+	public ResponseEntity<?> getProduct(@PathVariable("id") String id)
+	{
+		try
+		{
+			ProductModel order = service.findProductById(id);
+			if(order == null)
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			else
+				return new ResponseEntity<>(order, HttpStatus.OK);
+		}
+		catch(Exception e)
+		{
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
